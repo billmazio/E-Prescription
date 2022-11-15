4440514f013eae65611a38dc76d9544f4895fb58
billmazio <bmaziotis@windowslive.com>
2022-11-15 16:19:59 +0200

    PatientDrugServiceImpl v.0.1

---
+++ src/main/java/com/eprescription/eprescription/service/PatientDrugService.java
@@ -0,0 +1,9 @@
+package com.eprescription.eprescription.service;
+
+import com.eprescription.eprescription.entity.PatientDrug;
+
+public interface PatientDrugService {
+
+    PatientDrug savePatientDrug(PatientDrug pDrug);
+
+}
